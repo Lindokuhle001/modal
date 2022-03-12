807ef6e8205533590d4729b56718b0cb877c130c
Lindokuhle shabalala <mlixshaba@gmail.com>
2022-03-12 23:43:26 +0200

    added theme changer

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const modal = document.getElementsByClassName("modal-overlay")[0];
+const modal = document.getElementsByClassName("modal")[0];
+const modalOverlay = document.getElementsByClassName("modal-overlay")[0];
 const toggleModal = document.getElementsByClassName("modal-btn");
-
+const toggleTheme = document.getElementById("theme")
 
 for (const btn of toggleModal) {
   btn.addEventListener("click", () => {
-    modal.classList.toggle("hide");
+    modalOverlay.classList.toggle("hide");
   });
 }
+
+toggleTheme.addEventListener("change",()=>{
+    document.body.classList.toggle("body-dark")
+    modal.classList.toggle("modal-dark")
+})
